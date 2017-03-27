79297d9bb55595cc2998fe3a0d6acb896f8534d6
fretardloic <fretardloic@gmail.com>
2017-03-27 12:59:36 +0200

    Générateur de galaxie sommaire

--- maps/galaxy-generator.php
+++ maps/galaxy-generator.php
@@ -1,3 +1,21 @@
 <?php
+    session_start();
     
+    $playername = $_POST["playername"];
+    $players = ["new Player(\"$playername\")", "new Player(\"IA\")"];
+    $planets = ["new Planet(1, 10, 20, 20)",
+                "new Planet(2, 20, 30, 30, players[0])",
+                "new Planet(3, 30, 40, 40, players[1])"];
+    
+    ob_start();
+?>
+    
+    var players = [<?php echo join(", ", $players); ?>];
+    var planets = [<?php echo join(", ", $planets); ?>];
+    var galaxy = new Galaxy(planets, players);
+    
+<?php
+    $_SESSION["new-galaxy"] = ob_get_clean();
+    
+    header("Location : ../index.php");
 ?>
